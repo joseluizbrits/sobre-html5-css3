8f8a3079857650467c2a603b4e53766e2d5ef87b
José Luiz Brits <95440657+joseluizbrits@users.noreply.github.com>
2023-03-20 22:58:17 -0300

    Menu Dropdown

---
+++ menu-dropdown/js/app.js
@@ -0,0 +1,21 @@
+document.addEventListener('click', e => {
+    const isDropdownBtn = e.target.classList.contains('dropdown')
+
+    if (!isDropdownBtn && e.target.closest('.navbar__item') !== null) return
+
+    let currentSubmenu
+
+    if (isDropdownBtn) {
+        const item = e.target.closest('.navbar__item')
+
+        currentSubmenu = item.querySelector('.submenu')
+        
+        currentSubmenu.classList.toggle('show')
+    }
+
+    document.querySelectorAll('.submenu.show').forEach(submenu => {
+        if (submenu == currentSubmenu) return
+
+        submenu.classList.remove('show')
+    })
+})
